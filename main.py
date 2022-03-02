e522bc3c5e7fb15b0f63c9c73b76aea59e6797d8
jeanne-z <100534689+jeanne-z@users.noreply.github.com>
2022-03-02 13:57:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ width = 32
 height = 64
 
 array = np.zeros([height, width, 3], dtype=np.uint8)
-array[:,:] = [255, 128, 0]
+#array[:, :] = [255, 128, 0]
+index_map = []
+for j in range(height):
+    for i in range(width):
+        if not j % 2:
+            index_map[j * width + i] = (i, j)
+        else:
+            index_map[(j + 1) * width - i - 1] = (i, j)
+
+# wspolrzedne dla indexu
+print(index_map[123][0], index_map[123, 1])
 
 from PIL import Image
 
 img = Image.fromarray(array)
-img.save('testrgb.png')
-
-
+#img.save('testrgb.png')
